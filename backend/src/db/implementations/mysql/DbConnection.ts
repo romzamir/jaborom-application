83197510f2a8185a6709b9a252937f4ff1cc39cb
rom <romzamirr@gmail.com>
2021-05-22 20:31:19 +0300

    Added implementation to the MySqlDbConnection class

--- backend/src/db/implementations/mysql/DbConnection.ts
+++ backend/src/db/implementations/mysql/DbConnection.ts
@@ -1,13 +1,80 @@
+import { createConnection, Connection } from 'mysql';
+
 import IDbConnection from '../../abstractions/DbConnection';
 
 export default class MySqlDbConnection implements IDbConnection {
-    connect(config?: any): Promise<any> {
-        throw new Error('Method not implemented.');
+    private connected: boolean;
+    private connection: Connection | null;
+
+    constructor() {
+        this.connected = false;
+        this.connection = null;
     }
-    disconnect(): Promise<any> {
-        throw new Error('Method not implemented.');
+
+    connect(config: {
+        hostname: string;
+        port?: number;
+        user: string;
+        pass: string;
+        database?: string;
+    }): Promise<void> {
+        this.connection = createConnection({
+            host: config.hostname,
+            port: config.port,
+            user: config.user,
+            password: config.pass,
+            database: config.database,
+        });
+
+        this.connected = false;
+        return new Promise((resolve, reject) => {
+            if (!this.connection) {
+                reject('Failed to create a connection object.');
+                return;
+            }
+
+            this.connection.connect((err) => {
+                if (!!err) {
+                    reject(err);
+                } else {
+                    this.connected = true;
+                    resolve();
+                }
+            });
+        });
     }
+
+    disconnect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.connection?.end((err) => {
+                if (!!err) {
+                    reject(err);
+                } else {
+                    this.connected = false;
+                    resolve();
+                }
+            });
+        });
+    }
+
     isConnected(): Promise<boolean> {
-        throw new Error('Method not implemented.');
+        return Promise.resolve(this.connected);
+    }
+
+    query(sql: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            if (!this.connection) {
+                reject('Not connected!');
+                return;
+            }
+
+            this.connection.query(sql, (err, result) => {
+                if (!!err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
     }
 }
