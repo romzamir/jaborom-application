63102c8b784c3763c91c3d604e55fe10ffadc199
rom <romzamirr@gmail.com>
2021-05-22 20:05:05 +0300

    Added the login router

--- backend/src/bootstrapper.ts
+++ backend/src/bootstrapper.ts
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
-export default async function Boot() {
+//#region Routers
+import { LoginRouter } from './routers/login.router';
+//#endregion
+
+export default async function Boot(): Promise<Router> {
     const router = Router();
+
+    //#region Routers
+    const loginRouter = await LoginRouter();
+    //#endregion
+
+    router.use('/login', loginRouter);
 
     return router;
 }
