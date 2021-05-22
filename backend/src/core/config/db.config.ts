36e582db11d2e708f2284b73608f2dd81f601da2
rom <romzamirr@gmail.com>
2021-05-22 20:38:06 +0300

    Added a db config file

---
+++ backend/src/core/config/db.config.ts
@@ -0,0 +1,9 @@
+export const HOSTNAME: string = 'panel.hostar.cloud';
+
+export const PORT: number = 3306;
+
+export const USER: string = 'u47_EeZGIHvA9G';
+
+export const PASSWORD: string = '4=dMPoi^.E.jd@8QuKkq3w0A';
+
+export const DATABASE: string = 's47_romtotach';
