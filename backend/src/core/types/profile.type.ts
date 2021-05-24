6a6cb5703bf4aa42003a39d11507c58349ef9d3d
Rom Zamir <romzamir@gmail.com>
2021-05-24 21:10:55 +0300

    Created a Profile type

---
+++ backend/src/core/types/profile.type.ts
@@ -0,0 +1,12 @@
+export type Profile = {
+    id?: number;
+    personId: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+    dateOfSigning: Date;
+    address: string;
+    hobbies: string;
+    allergies: string;
+    notes: string;
+};
