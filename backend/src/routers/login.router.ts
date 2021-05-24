8fe790d2db2ee0b95138eaab646b2377840699c2
rom <romzamirr@gmail.com>
2021-05-24 16:51:03 +0300

    Added a dummy /login route

--- backend/src/routers/login.router.ts
+++ backend/src/routers/login.router.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import HttpStatus from 'http-status';
 
 export async function LoginRouter(): Promise<Router> {
     const router = Router();
+
+    router.get('/', (req, res) => {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        res.end();
+    });
 
     return router;
 }
