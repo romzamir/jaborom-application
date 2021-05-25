7b4d47eaa8be28f154bd8c2313fe4d452807a150
Rom Zamir <romzamir@gmail.com>
2021-05-25 23:14:15 +0300

    Added a POST /profiles route to create a new profile

--- backend/src/routers/profiles.router.ts
+++ backend/src/routers/profiles.router.ts
@@ -1,4 +1,7 @@
 import { Request, Response, Router } from 'express';
+import HttpStatus from 'http-status';
+
+import { Profile } from '../core/types/profile.type';
 import IProfilesDbTable from '../db/abstractions/types/profiles.dbTable';
 
 export default function ProfilesRouter(dbTable: IProfilesDbTable): Router {
@@ -12,5 +15,14 @@ export default function ProfilesRouter(dbTable: IProfilesDbTable): Router {
         res.json(profiles);
     });
 
+    router.post('/', async (req, res) => {
+        const profile: Profile = req.body;
+        try {
+            const result = await dbTable.insertProfile(profile);
+            res.json(result);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    });
     return router;
 }
