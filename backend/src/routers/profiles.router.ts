ec469b8a1cfb9b49b1b8891441d2d0832213afb0
Rom Zamir <romzamir@gmail.com>
2021-05-25 22:50:19 +0300

    Added a /profiles route to get all profiles

--- backend/src/routers/profiles.router.ts
+++ backend/src/routers/profiles.router.ts
@@ -4,5 +4,13 @@ import IProfilesDbTable from '../db/abstractions/types/profiles.dbTable';
 export default function ProfilesRouter(dbTable: IProfilesDbTable): Router {
     const router = Router();
 
+    router.get('/', async (req, res) => {
+        const includeGraduates = (req.query.includeGraduate ||
+            false) as boolean;
+
+        const profiles = await dbTable.getProfiles({ includeGraduates });
+        res.json(profiles);
+    });
+
     return router;
 }
