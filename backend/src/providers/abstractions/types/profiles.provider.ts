05d1ff8f38c51d54404a01d730d4db9ff604e57e
Rom Zamir <romzamir@gmail.com>
2021-06-01 09:24:12 +0300

    Added functions to the IProfilesProvider interface

--- backend/src/providers/abstractions/types/profiles.provider.ts
+++ backend/src/providers/abstractions/types/profiles.provider.ts
@@ -2,5 +2,8 @@ import { Profile } from '../../../core/types/profile.type';
 import IDataProvider from '../provider';
 
 export default interface IProfilesProvider extends IDataProvider<Profile> {
-    
+    getAllProfiles(): Promise<Profile[]>;
+    getProfileByID(id: string): Promise<Profile | null>;
+    insertProfile(profile: Profile): Promise<Profile | null>;
+    deleteProfile(id: string): Promise<Profile | null>;
 }
