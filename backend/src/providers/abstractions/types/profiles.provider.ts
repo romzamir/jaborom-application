3ba4d379d3de2b0370ea26b03bf9482fb43aef45
Rom Zamir <romzamir@gmail.com>
2021-06-01 09:23:49 +0300

    Created an IProfilesProvider interface

---
+++ backend/src/providers/abstractions/types/profiles.provider.ts
@@ -0,0 +1,6 @@
+import { Profile } from '../../../core/types/profile.type';
+import IDataProvider from '../provider';
+
+export default interface IProfilesProvider extends IDataProvider<Profile> {
+    
+}
