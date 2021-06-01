54e36c73ef6a211b31321ed20522c347a5f271a4
Rom Zamir <romzamir@gmail.com>
2021-06-01 09:46:45 +0300

    Changed the signatures of some functions

--- backend/src/providers/abstractions/types/profiles.provider.ts
+++ backend/src/providers/abstractions/types/profiles.provider.ts
@@ -2,8 +2,11 @@ import { Profile } from '../../../core/types/profile.type';
 import IDataProvider from '../provider';
 
 export default interface IProfilesProvider extends IDataProvider {
-    getAllProfiles(): Promise<Profile[]>;
-    getProfileByID(id: string): Promise<Profile | null>;
+    getAllProfiles(includeGraduates?: boolean): Promise<Profile[]>;
+    getProfileByID(
+        id: number,
+        includeGraduates?: boolean
+    ): Promise<Profile | null>;
     insertProfile(profile: Profile): Promise<Profile | null>;
-    deleteProfile(id: string): Promise<Profile | null>;
+    deleteProfile(id: number): Promise<boolean>;
 }
