1aea8dc357045e1370bc91cfb8ae2f02b2e69153
Rom Zamir <romzamir@gmail.com>
2021-06-11 14:51:08 +0300

    Created basic text

--- frontend/src/components/homePage/homePage.tsx
+++ frontend/src/components/homePage/homePage.tsx
@@ -4,5 +4,16 @@ import { AppPage, onPageChosenFunction } from '../../types/page.type';
 import './homePage.css';
 
 export function HomePage(props: { onPageChosen: onPageChosenFunction }) {
-    return <></>;
+    return (
+        <div className="home-page">
+            <span className="home-page-clock">14:32</span>
+            <div className="home-page-text">
+                <div className="home-page-hello-container">
+                    שלום,
+                    <span className="home-page-hello-name">רום</span>
+                </div>
+                מה תרצה לעשות?
+            </div>
+        </div>
+    );
 }
