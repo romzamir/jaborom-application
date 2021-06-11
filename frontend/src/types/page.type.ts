b49455465103b714c43affdb7534608882177a2d
Rom Zamir <romzamir@gmail.com>
2021-06-11 18:11:20 +0300

    Created a page component type

--- frontend/src/types/page.type.ts
+++ frontend/src/types/page.type.ts
@@ -1,3 +1,7 @@
+import { ReactElement } from 'react';
+
 export type AppPage = 'PROFILES' | 'FEEDBACKS' | 'BUDGET' | 'MANAGEMENT';
 
 export type OnPageChosenFunction = (page: AppPage) => void;
+
+export type PageComponent = (props?: any) => JSX.Element;
