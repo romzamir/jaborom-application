c8c56eb7ab5411af5a64b8e1c9933f28369a5eaf
Rom Zamir <romzamir@gmail.com>
2021-06-11 15:01:02 +0300

    The clock now updates

--- frontend/src/components/homePage/homePage.tsx
+++ frontend/src/components/homePage/homePage.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppPage, onPageChosenFunction } from '../../types/page.type';
 
 import './homePage.css';
 
 export function HomePage(props: { onPageChosen: onPageChosenFunction }) {
+    const [clockText, setClockText] = useState(generateTimeText());
+
+    function generateTimeText() {
+        const now = new Date();
+        const hours = now.getHours();
+        const hoursText = (hours < 10 ? '0' : '') + hours;
+        const minutes = now.getMinutes();
+        const minutesText = (minutes < 10 ? '0' : '') + minutes;
+        return `${hoursText}:${minutesText}`;
+    }
+
+    const updateClockText = () => {
+        const newText = generateTimeText();
+        if (newText === clockText) return;
+
+        setClockText(newText);
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(updateClockText, 1000);
+
+        return function () {
+            clearInterval(intervalId);
+        };
+    }, []);
+
     return (
         <div className="home-page">
-            <span className="home-page-clock">14:32</span>
+            <span className="home-page-clock">{clockText}</span>
             <div className="home-page-text">
                 <div className="home-page-hello-container">
                     שלום,
