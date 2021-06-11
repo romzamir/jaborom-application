a1474ff9020581312d73da784194b4b93ce59d70
Rom Zamir <romzamir@gmail.com>
2021-06-11 14:36:20 +0300

    Added global css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { HomePage } from './components/homePage/homePage';
 import { AppPage } from './types/page.type';
 
+import './App.css';
+
 export default function App() {
     const onPageChosen = (page: AppPage) => {};
 
