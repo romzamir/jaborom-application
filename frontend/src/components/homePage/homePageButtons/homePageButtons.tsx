bb5f3ac9a44d47a4e7eb8214c9ef7cb9cb7d88f0
Rom Zamir <romzamir@gmail.com>
2021-06-11 15:53:49 +0300

    Now rendering the HomePageButton components with the values

--- frontend/src/components/homePage/homePageButtons/homePageButtons.tsx
+++ frontend/src/components/homePage/homePageButtons/homePageButtons.tsx
@@ -1,9 +1,21 @@
 import { HomePageButton } from './homePageButton/homePageButton';
 
+import ProfilesSvg from './images/profiles.svg';
+import FeedbackSvg from './images/feedback.svg';
+import BudgetSvg from './images/budget.svg';
+import ManagementSvg from './images/management.svg';
+
 import { OnPageChosenFunction } from '../../../types/page.type';
 
 import './homePageButtons.css';
 
 export function HomePageButtons(props: { onPageChosen: OnPageChosenFunction }) {
-    return <></>;
+    return (
+        <div className="home-page-buttons">
+            <HomePageButton icon={ProfilesSvg} title="טפסי קליטה" />
+            <HomePageButton icon={FeedbackSvg} title="משוב" />
+            <HomePageButton icon={BudgetSvg} title="תקציב" />
+            <HomePageButton icon={ManagementSvg} title="ניהול" />
+        </div>
+    );
 }
