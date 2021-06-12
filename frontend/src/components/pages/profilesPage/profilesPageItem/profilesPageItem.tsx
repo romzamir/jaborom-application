a945c0ce69772e48a9cb10b419217f9930e0e403
Rom Zamir <romzamir@gmail.com>
2021-06-12 13:07:08 +0300

    Created a profiles page item component

---
+++ frontend/src/components/pages/profilesPage/profilesPageItem/profilesPageItem.tsx
@@ -0,0 +1,7 @@
+import { Profile } from '../../../../types/profile.type';
+
+import './profilesPageItem.css';
+
+export function ProfilesPageItem(props: { profile: Profile }) {
+    return <></>;
+}
