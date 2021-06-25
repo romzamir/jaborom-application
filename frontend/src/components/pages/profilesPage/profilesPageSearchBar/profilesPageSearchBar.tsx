1ab18b01dbb77a7b0888226a99cfef0ac520d098
Rom Zamir <romzamir@gmail.com>
2021-06-25 16:48:45 +0300

    Added the style for the profiles page search bar

--- frontend/src/components/pages/profilesPage/profilesPageSearchBar/profilesPageSearchBar.tsx
+++ frontend/src/components/pages/profilesPage/profilesPageSearchBar/profilesPageSearchBar.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
+import SearchSvg from './search.svg';
+
 import './profilesPageSearchBar.css';
 
 export function ProfilesPageSearchBar() {
-    return <></>;
+    const [value, setValue] = useState('');
+
+    const onValueChanged = (event: any) => {
+        const newValue = event.target.value;
+        setValue(newValue);
+        console.log(newValue);
+    };
+
+    return (
+        <div className="profiles-page-search-bar">
+            <input
+                className="profiles-page-search-bar-input"
+                value={value}
+                onChange={onValueChanged}
+                placeholder="חיפוש"
+            />
+            <div className="profiles-page-search-bar-separator"></div>
+            <img className="profiles-page-search-bar-button" src={SearchSvg} />
+        </div>
+    );
 }
