4507c66a39c5f134a7bf4175fde9a34cd22e01a6
Rom Zamir <romzamir@gmail.com>
2021-10-01 14:38:24 +0300

    Added the pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {HomePage} from './components/pages/homePage/homePage';
 import {ProfilesPage} from './components/pages/profilesPage';
+
+import './App.css';
 
 export default function App() {
     return (
         <div className='App'>
-            <PagesHandler>
-                <div></div>
-                <ProfilesPage name='PROFILES'></ProfilesPage>
-            </PagesHandler>
+            <Router>
+                <Switch>
+                    <Route path='/profiles'>
+                        <ProfilesPage />
+                    </Route>
+                    <Route path='/'>
+                        <HomePage></HomePage>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
