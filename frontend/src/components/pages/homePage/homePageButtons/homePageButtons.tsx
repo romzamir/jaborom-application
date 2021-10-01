298c880afa55625f32d861c21e8d8de8e0f768ef
Rom Zamir <romzamir@gmail.com>
2021-10-01 14:17:07 +0300

    Now using the router's links

--- frontend/src/components/pages/homePage/homePageButtons/homePageButtons.tsx
+++ frontend/src/components/pages/homePage/homePageButtons/homePageButtons.tsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 import {HomePageButton} from './homePageButton/homePageButton';
 
 import ProfilesSvg from './images/profiles.svg';
@@ -10,26 +11,34 @@ import './homePageButtons.css';
 export function HomePageButtons() {
     return (
         <div className='home-page-buttons'>
-            <HomePageButton
-                color='#E88E3C'
-                icon={ProfilesSvg}
-                title='חניכים'
-            />
-            <HomePageButton
-                color='#3CAAE8'
-                icon={FeedbackSvg}
-                title='משוב'
-            />
-            <HomePageButton
-                color='#E83C5B'
-                icon={BudgetSvg}
-                title='תקציב'
-            />
-            <HomePageButton
-                color='#3CE8BE'
-                icon={ManagementSvg}
-                title='ניהול'
-            />
+            <Link to='profiles'>
+                <HomePageButton
+                    color='#E88E3C'
+                    icon={ProfilesSvg}
+                    title='חניכים'
+                />
+            </Link>
+            <Link to='feedback'>
+                <HomePageButton
+                    color='#3CAAE8'
+                    icon={FeedbackSvg}
+                    title='משוב'
+                />
+            </Link>
+            <Link to='financial'>
+                <HomePageButton
+                    color='#E83C5B'
+                    icon={BudgetSvg}
+                    title='תקציב'
+                />
+            </Link>
+            <Link to='admin'>
+                <HomePageButton
+                    color='#3CE8BE'
+                    icon={ManagementSvg}
+                    title='ניהול'
+                />
+            </Link>
         </div>
     );
 }
