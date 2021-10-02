abba682110eff0ed493487d029add3618579f533
Rom Zamir <romzamir@gmail.com>
2021-10-02 17:22:11 +0300

    Now getting the ID from the params

--- frontend/src/components/pages/profilePage/profilePage.tsx
+++ frontend/src/components/pages/profilePage/profilePage.tsx
@@ -1,5 +1,12 @@
+import {useParams} from 'react-router-dom';
+
 import './profilePage.css';
 
 export function ProfilePage() {
+    const {id} = useParams<ProfileParams>();
     return <></>;
 }
+
+type ProfileParams = {
+    id: string;
+};
