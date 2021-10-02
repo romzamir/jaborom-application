0cd432b0e8aea482f215bf0da2a425d2c5cfefdf
Rom Zamir <romzamir@gmail.com>
2021-10-02 17:11:02 +0300

    Changed path '/' to exact

--- frontend/src/components/pagesTransitions/pagesTransitions.tsx
+++ frontend/src/components/pagesTransitions/pagesTransitions.tsx
@@ -16,6 +16,7 @@ export function PagesTransitions() {
                         <ProfilesPage />
                     </Route>
                     <Route path='/'>
+                    <Route exact path='/'>
                         <HomePage></HomePage>
                     </Route>
                     <Route path='*'>
