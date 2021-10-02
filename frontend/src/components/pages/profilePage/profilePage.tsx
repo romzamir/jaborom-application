23e4629e843a425485fb6f9b9b91fb4289f117b8
Rom Zamir <romzamir@gmail.com>
2021-10-02 19:26:51 +0300

    Now getting the profile

--- frontend/src/components/pages/profilePage/profilePage.tsx
+++ frontend/src/components/pages/profilePage/profilePage.tsx
@@ -1,12 +1,30 @@
-import {useState} from 'react';
-import {useParams} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import {useParams, useHistory} from 'react-router-dom';
+import {AxiosResponse} from 'axios';
+
+import {Profile} from 'core/types/profile.type';
+import {profilesProvider} from 'api/providers/profiles.provider';
 
 import './profilePage.css';
 
 export function ProfilePage() {
     const {id} = useParams<ProfileParams>();
+    const [profile, setProfile] = useState<Profile | null>();
     const [isLoading, setIsLoading] = useState(true);
-    return isLoading && <div className='profile-page'></div>;
+
+    useEffect(() => {
+        setIsLoading(true);
+        const promise = profilesProvider.getProfile(id);
+        promise.then(profileReceived);
+        return promise.cancel;
+    }, [id]);
+
+    const profileReceived = (response: AxiosResponse<Profile>) => {
+        setIsLoading(false);
+        setProfile(response.data ?? null);
+    };
+
+    return isLoading ? <div>LOADING...</div> : <div className='profile-page'>{profile?.firstName}</div>;
 }
 
 type ProfileParams = {
