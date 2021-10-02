516d90d7dbd98350fe115a80b6bb4da12b12b698
Rom Zamir <romzamir@gmail.com>
2021-10-02 12:18:28 +0300

    Updated profile type

--- backend/src/core/types/profile.type.ts
+++ backend/src/core/types/profile.type.ts
@@ -4,6 +4,9 @@ export type Profile = {
     personId?: number;
     firstName: string;
     lastName: string;
+    school: number;
+    grade: number;
+    sex: boolean;
     dateOfBirth?: Date;
     dateOfSigning: Date;
     address?: string;
