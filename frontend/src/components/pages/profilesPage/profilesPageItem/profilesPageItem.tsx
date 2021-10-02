30f81568454f4f4dd64d312250ebf3ca43b6b4ed
Rom Zamir <romzamir@gmail.com>
2021-10-02 17:05:02 +0300

    When clicking a profile now redirecting to its route

--- frontend/src/components/pages/profilesPage/profilesPageItem/profilesPageItem.tsx
+++ frontend/src/components/pages/profilesPage/profilesPageItem/profilesPageItem.tsx
@@ -1,3 +1,4 @@
+import {Redirect, Link} from 'react-router-dom';
 import {Profile} from 'core/types/profile.type';
 
 import './profilesPageItem.css';
@@ -11,19 +12,15 @@ export function ProfilesPageItem(props: {profile: Profile}) {
 
     return (
         <div className='profiles-page-item-container'>
-            <h2 className='profiles-page-item-name'>{name}</h2>
-            <h4 className='profiles-page-item-id'>{id}</h4>
-            <div className='profiles-page-item-tags'>
-                <span className='profiles-page-item-tag profiles-page-item-sex'>
-                    {sex}
-                </span>
-                <span className='profiles-page-item-tag profiles-page-item-grade'>
-                    {grade}
-                </span>
-                <span className='profiles-page-item-tag profiles-page-item-school'>
-                    {school}
-                </span>
-            </div>
+            <Link to='' style={{textDecoration: 'none'}}>
+                <h2 className='profiles-page-item-name'>{name}</h2>
+                <h4 className='profiles-page-item-id'>{id}</h4>
+                <div className='profiles-page-item-tags'>
+                    <span className='profiles-page-item-tag profiles-page-item-sex'>{sex}</span>
+                    <span className='profiles-page-item-tag profiles-page-item-grade'>{grade}</span>
+                    <span className='profiles-page-item-tag profiles-page-item-school'>{school}</span>
+                </div>
+            </Link>
         </div>
     );
 }
