da338ab723879643ff9c3e39f07e9b7f99aee6e4
Rom Zamir <romzamir@gmail.com>
2021-10-02 16:18:36 +0300

    Now performing the search when rendered

--- frontend/src/components/pages/profilesPage/profilesPage.tsx
+++ frontend/src/components/pages/profilesPage/profilesPage.tsx
@@ -1,11 +1,15 @@
-import {useState} from 'react';
+import _ from 'lodash';
+import {useEffect, useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import {ProfilesPageItem} from './profilesPageItem';
 import {ProfilesPageSearchBar} from './profilesPageSearchBar';
 
 import {Profile} from 'core/types/profile.type';
 
+import {profilesProvider} from 'api/providers/profiles.provider';
+
 import './profilesPage.css';
+import {AxiosResponse} from 'axios';
 
 const exampleProfile: Profile = {
     id: 1,
@@ -18,12 +22,36 @@ const exampleProfile: Profile = {
 };
 
 export function ProfilesPage() {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [profiles, setProfiles] = useState<Profile[]>([]);
     const location = useLocation();
-    console.log(location);
+
+    useEffect(() => {
+        const searchText = getSearchText();
+        const searchPromise = performSearch(searchText);
+        searchPromise.then(onSearchResponse);
+        return searchPromise.cancel;
+    }, [location]);
 
     const getSearchText = () => {
         return new URLSearchParams(location.search).get('search') || '';
+    };
+
+    const performSearch = (searchText: string) => {
+        if (_.isEmpty(searchText)) {
+            return profilesProvider.getAllProfiles();
+        }
+
+        return profilesProvider.searchProfiles(searchText);
+    };
+
+    const onSearchResponse = (response: AxiosResponse<Profile[]>) => {
+        if (response.status === 200) {
+            setProfiles(response.data);
+            setIsLoading(false);
+        }
+
+        console.log('Response', response);
     };
 
     return (
