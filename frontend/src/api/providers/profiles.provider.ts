914475dc4b049f5e55fbf001b00bacd2f10ac540
Rom Zamir <romzamir@gmail.com>
2022-09-26 17:55:03 +0300

    Creating a profile object of class Profile when fetching a profile

--- frontend/src/api/providers/profiles.provider.ts
+++ frontend/src/api/providers/profiles.provider.ts
@@ -1,8 +1,9 @@
 import {restCommunicator} from '../../api/restCommunicator';
 import {backendRoutes} from '../../core/constants/backendRoutes.constants';
 
-import {Profile} from '../../core/types/profile.type';
+import {Profile} from '../../core/models/profile';
 import {ProfileDb} from '../../core/types/profileDb.type';
+import {wrapCancelablePromise} from '../../utils/cancelablePromise';
 
 export const profilesProvider = ((window as any).profilesProvider = {
     getAllProfiles,
@@ -15,7 +16,15 @@ function getAllProfiles(limit: number = 20) {
 }
 
 function getProfile(id: number | string) {
-    return restCommunicator.get<Profile>(`${backendRoutes.profiles}/${id}`);
+    return wrapCancelablePromise(
+        restCommunicator.get<Profile>(`${backendRoutes.profiles}/${id}`),
+        async (promise) => {
+            const response = await promise;
+            if (!response.data) return;
+
+            response.data = new Profile(response.data);
+        },
+    );
 }
 
 function searchProfiles(searchText: string, limit: number = 20) {
