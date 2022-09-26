09c676035e558554b6ad6eaaeacd29bc6ca62995
Rom Zamir <romzamir@gmail.com>
2022-09-26 18:00:04 +0300

    Added the date that the profile was created at

--- frontend/src/components/pages/profilePage/header/Header.tsx
+++ frontend/src/components/pages/profilePage/header/Header.tsx
@@ -1,5 +1,7 @@
 import {Profile} from '../../../../core/models/profile';
 
+import {dateToLongDateString} from '../../../../utils/date';
+
 import './header.css';
 
 type ProfilePageHeaderProps = {
@@ -9,10 +11,17 @@ type ProfilePageHeaderProps = {
 export function ProfilePageHeader({profile}: ProfilePageHeaderProps) {
     return (
         <div className='profile-page-header'>
-            <span className='profile-name'>
-                {profile.firstName} {profile.lastName}
-            </span>
-            <span className='profile-person-id'>{profile.personId}</span>
+            <div className='profile-page-header-main'>
+                <span className='profile-name'>
+                    {profile.firstName} {profile.lastName}
+                </span>
+                <span className='profile-person-id'>{profile.personId}</span>
+            </div>
+            <div className='profile-page-header-extras'>
+                <span className='profile-sign-date'>
+                    הפרופיל נוצר ב{dateToLongDateString(profile.dateOfSigning)}
+                </span>
+            </div>
         </div>
     );
 }
