2fc2670672ed60f2a49adf9d013633c95fc033ac
Rom Zamir <romzamir@gmail.com>
2022-09-26 18:21:13 +0300

    Added toggle field type

--- frontend/src/components/pages/profilePage/field/fields/index.tsx
+++ frontend/src/components/pages/profilePage/field/fields/index.tsx
@@ -2,13 +2,14 @@ import {ProfileDateField} from './Date';
 import {ProfileLongTextField} from './LongText';
 import {ProfileTextField} from './Text';
 
-export type FieldType = 'Text' | 'Long Text' | 'Date';
+export type FieldType = 'Date' | 'Text' | 'Long Text' | 'Toggle';
 
 export const fieldsComponentsByNames: Record<
     FieldType,
     (value: any) => JSX.Element
 > = {
+    Date: (value: Date) => <ProfileDateField value={value} />,
     Text: (value: string) => <ProfileTextField value={value} />,
     'Long Text': (value: string) => <ProfileLongTextField value={value} />,
-    Date: (value: Date) => <ProfileDateField value={value} />,
+    Toggle: (value: string) => <ProfileTextField value={value} />,
 };
