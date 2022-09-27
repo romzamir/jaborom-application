0ffedc843da742e5916a4b98ec19554bb09db66e
Rom Zamir <romzamir@gmail.com>
2022-09-27 12:36:19 +0300

    Added edit mode to the long text

--- frontend/src/components/pages/profilePage/field/fields/LongText.tsx
+++ frontend/src/components/pages/profilePage/field/fields/LongText.tsx
@@ -10,6 +10,22 @@ export type ProfileLongTextFieldProps = {
 export function ProfileLongTextField({
     title,
     value = '',
+    setValue,
+    isEditMode,
 }: ProfileLongTextFieldProps) {
-    return <ProfileFieldContainer title={title}>{value}</ProfileFieldContainer>;
+    return (
+        <ProfileFieldContainer
+            title={title}
+            className='profile-long-text-field'
+        >
+            {isEditMode ? (
+                <textarea
+                    value={value}
+                    onInput={(event) => setValue(event.currentTarget.value)}
+                />
+            ) : (
+                value
+            )}
+        </ProfileFieldContainer>
+    );
 }
