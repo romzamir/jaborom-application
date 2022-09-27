5caa42aa9db2454a7c9991a39c28ffe87cea71f6
Rom Zamir <romzamir@gmail.com>
2022-09-27 12:11:02 +0300

    Changed to text field

--- frontend/src/components/pages/profilePage/body/Body.tsx
+++ frontend/src/components/pages/profilePage/body/Body.tsx
@@ -2,6 +2,7 @@ import {Profile} from '../../../../core/models/profile';
 import {
     ProfileDateField,
     ProfileLongTextField,
+    ProfileTextField,
     ProfileToggleField,
 } from '../field';
 
@@ -38,7 +39,7 @@ export function ProfilePageBody({
                 options={SEX_OPTIONS}
                 isEditMode={isEditMode}
             />
-            <ProfileLongTextField
+            <ProfileTextField
                 title='כתובת'
                 value={profile.address}
                 setValue={(value) => setField('address', value)}
