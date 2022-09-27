5628deac9dae78e7faf1e93accc2d84dc12eff10
Rom Zamir <romzamir@gmail.com>
2022-09-27 12:49:47 +0300

    Added date picker when in edit mode

--- frontend/src/components/pages/profilePage/field/fields/Date.tsx
+++ frontend/src/components/pages/profilePage/field/fields/Date.tsx
@@ -1,5 +1,9 @@
+import DatePicker from 'react-datepicker';
+
 import {dateToLongDateString} from '../../../../../utils/date';
 import {ProfileFieldContainer} from '../field';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 export type ProfileDateFieldProps = {
     title: string;
@@ -8,10 +12,24 @@ export type ProfileDateFieldProps = {
     isEditMode: boolean;
 };
 
-export function ProfileDateField({title, value}: ProfileDateFieldProps) {
+export function ProfileDateField({
+    title,
+    value,
+    setValue,
+    isEditMode,
+}: ProfileDateFieldProps) {
     return (
         <ProfileFieldContainer title={title}>
-            {dateToLongDateString(value)}
+            {isEditMode ? (
+                <DatePicker
+                    wrapperClassName='profile-date-field'
+                    className='profile-date-field-input'
+                    selected={value}
+                    onChange={setValue}
+                />
+            ) : (
+                dateToLongDateString(value)
+            )}
         </ProfileFieldContainer>
     );
 }
