222a3f39f41479ebfd86fe6ec50e250a2c19dd6a
Rom Zamir <romzamir@gmail.com>
2022-10-07 11:03:05 +0300

    Navigating to the home page after authentication

--- frontend/src/components/pages/login/LoginPage.tsx
+++ frontend/src/components/pages/login/LoginPage.tsx
@@ -1,11 +1,29 @@
+import {useEffect} from 'react';
+import {User} from 'firebase/auth';
 import GoogleButton from 'react-google-button';
 
 import {authenticator} from '../../../auth';
+import {useNavigate} from 'react-router-dom';
 
 import './loginPage.css';
 
 export function LoginPage() {
-    return <></>;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const removeListener = authenticator.onUserChanged(onUserChanged);
+
+        return () => {
+            removeListener();
+        };
+    }, []);
+
+    const onUserChanged = (user: User | null) => {
+        if (!user) return;
+
+        navigate('/homepage');
+    };
+
     return (
         <div className='login-page'>
             <GoogleButton
