c34a6146797d345fb6fc107a848b78fa4b5c9572
Rom Zamir <romzamir@gmail.com>
2022-10-07 11:02:53 +0300

    Added google button

--- frontend/src/components/pages/login/LoginPage.tsx
+++ frontend/src/components/pages/login/LoginPage.tsx
@@ -1,5 +1,18 @@
+import GoogleButton from 'react-google-button';
+
+import {authenticator} from '../../../auth';
+
 import './loginPage.css';
 
 export function LoginPage() {
     return <></>;
+    return (
+        <div className='login-page'>
+            <GoogleButton
+                className='login-in-with-google'
+                label='התחברות עם Google'
+                onClick={authenticator.signInWithGoogle}
+            />
+        </div>
+    );
 }
