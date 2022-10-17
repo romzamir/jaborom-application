2f5283ba5c1fa8245495e9f6b4ec666bf34a68f3
Salenia <vsreui123456@gmail.com>
2022-10-17 23:31:21 +0200

    Simplify definition of models/Profile

--- apps/web/client/src/core/models/profile.ts
+++ apps/web/client/src/core/models/profile.ts
@@ -3,62 +3,21 @@ import {ProfileSex} from '../types/sex.type';
 import {profileSexToSexNumber, sexNumberToProfileSex} from '../../utils/sex';
 
 export class Profile {
-    public id: number;
-    public personId: string;
-    public firstName: string;
-    public lastName: string;
-    public school: number;
-    public grade: number;
-    public sex: ProfileSex;
-    public dateOfBirth: Date | null;
-    public dateOfSigning: Date;
-    public address: string;
-    public hobbies: string;
-    public allergies: string;
-    public notes: string;
-
-    constructor(template: any = {}) {
-        const {
-            id,
-            personId,
-            firstName,
-            lastName,
-            school,
-            grade,
-            sex,
-            dateOfBirth,
-            dateOfSigning,
-            address,
-            hobbies,
-            allergies,
-            notes,
-        }: {
-            id: number;
-            personId: string;
-            firstName: string;
-            lastName: string;
-            school: number;
-            grade: number;
-            sex: any;
-            dateOfBirth: any;
-            dateOfSigning: any;
-            address?: string;
-            hobbies?: string;
-            allergies?: string;
-            notes?: string;
-        } = template;
-
-        this.id = id;
-        this.personId = personId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.school = school;
-        this.grade = grade;
-        this.address = address ?? '';
-        this.hobbies = hobbies ?? '';
-        this.allergies = allergies ?? '';
-        this.notes = notes ?? '';
-
+    constructor(
+        public id: number,
+        public personId: string,
+        public firstName: string,
+        public lastName: string,
+        public school: number,
+        public grade: number,
+        public sex: ProfileSex,
+        public dateOfBirth: Date | null,
+        public dateOfSigning: Date,
+        public address: string = '',
+        public hobbies: string = '',
+        public allergies: string = '',
+        public notes: string = '',
+    ) {
         this.sex = typeof sex === 'number' ? sexNumberToProfileSex(sex) : sex;
         this.dateOfBirth =
             dateOfBirth instanceof Date
@@ -72,8 +31,8 @@ export class Profile {
                 : new Date(dateOfSigning);
     }
 
-    clone() {
-        return new Profile(this);
+    clone(): Profile {
+        return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
     }
 
     static toJson(profile: Partial<Profile>) {
