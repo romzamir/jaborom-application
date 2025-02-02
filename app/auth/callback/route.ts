e3fccb20452b98dfb5e96cb465ab3a2e711ae17e
Rom Zamir <romzamir@gmail.com>
2025-02-03 00:00:44 +0200

    Created the new project! With basic auth

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { createClient } from "@/utils/supabase/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: NextRequest) {
+  const supabase = await createClient();
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  console.log({ code });
+  if (!code) {
+    return NextResponse.error();
+  }
+
+  const { data, error: authError } = await supabase.auth.exchangeCodeForSession(
+    code
+  );
+
+  const nextUrl = new URL("/", url.origin);
+  return NextResponse.redirect(nextUrl);
+}
