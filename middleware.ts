a18f26cf3acf345be42d3aa7fd0e215d4354c305
Rom Zamir <romzamir@gmail.com>
2025-02-09 23:05:14 +0200

    Changed to the files from supabase's docs

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,19 @@
-import { authMiddleware } from "@/utils/supabase/middleware";
-import { NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await authMiddleware(request);
+  return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)", // Exclude Next.js assets
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function authMiddleware(request: NextRequest) {
+export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
