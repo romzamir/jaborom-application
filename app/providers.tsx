9cbb1a659e702e5689e571ca1c4620d1df0479fd
Rom Zamir <romzamir@gmail.com>
2025-02-15 01:57:54 +0200

    Made it generic

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import { ComponentType, PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { DirectionProvider } from "@radix-ui/react-direction";
 
@@ -8,12 +8,38 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 const queryClient = new QueryClient({});
 
+type ProviderEntry<T = any> = [ComponentType<T>, T];
+const providersToRender: ProviderEntry[] = [
+  [
+    QueryClientProvider,
+    {
+      client: queryClient,
+    },
+  ],
+  [
+    ThemeProvider,
+    { attribute: "class", defaultTheme: "dark", enableSystem: true },
+  ],
+  [
+    DirectionProvider,
+    {
+      dir: "rtl",
+    },
+  ],
+];
+
 export function Providers({ children }: PropsWithChildren) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <DirectionProvider dir="rtl">{children}</DirectionProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+  const wrapWithNext = (index: number) => {
+    if (index === providersToRender.length) {
+      return <>{children}</>;
+    }
+
+    const [CurrentProvider, props] = providersToRender[index];
+
+    return (
+      <CurrentProvider {...props}>{wrapWithNext(index + 1)}</CurrentProvider>
+    );
+  };
+
+  return wrapWithNext(0);
 }
