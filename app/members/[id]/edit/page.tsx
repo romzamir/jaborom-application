24344b2ec070cd9823c28ad4ab888be3a61d164b
Rom Zamir <romzamir@gmail.com>
2025-02-16 22:32:46 +0200

    Added weird skeleton

--- app/members/[id]/edit/page.tsx
+++ app/members/[id]/edit/page.tsx
@@ -1,29 +1,28 @@
+"use client";
+
 import { notFound } from "next/navigation";
-import { createClient } from "@/utils/supabase/server";
 import MemberForm from "@/components/member-form";
 import { Member } from "@/types/member";
+import { useMember } from "@/hooks/use-member";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default async function EditMemberPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const supabase = await createClient();
+export default function EditMemberPage({ params }: { params: { id: string } }) {
   const id = Number.parseInt(params.id);
-  const { data: member, error } = await supabase
-    .from("members")
-    .select("*")
-    .eq("id", id)
-    .single();
 
-  if (error || !member) {
-    notFound();
+  const { data: member, isLoading } = useMember(id);
+
+  if (!member && !isLoading) {
+    return notFound();
   }
 
   return (
     <div className="container mx-auto p-4 rtl">
       <h1 className="text-3xl font-bold mb-6 text-center">עריכת חבר</h1>
-      <MemberForm initialData={Member.parse(member)} />
+      {isLoading ? (
+        <Skeleton className="w-24 h-64 min-w-full min-h-full" />
+      ) : (
+        <MemberForm initialData={Member.parse(member)} />
+      )}
     </div>
   );
 }
